72fced6120985b8b5bcfa4776b0b9f825e4dfc02
B.H Yoo <pikabh@naver.com>
2024-11-21 14:02:34 +0900

    If it's not fixed yet, I don't know.

--- utils/crews.py
+++ utils/crews.py
@@ -5,7 +5,7 @@ from utils.logs import LoggerSetup
 
 logger = LoggerSetup("utils.crews").logger
 
-def set_openai_api_key_for_vision_tool(model_name):
+def set_openai_api_key_for_vision_tool(model_name: str) -> str:
     registered_openai_api_key = get_api_key(model_name)
     if registered_openai_api_key:
         logger.info("Set API key from customer registered API key.")
